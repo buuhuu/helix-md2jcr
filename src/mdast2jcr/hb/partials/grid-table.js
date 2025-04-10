e5d2139620fa00d76a4a051250e4c7eb9e0949a7
bhellema <benjaminhelleman@gmail.com>
2025-04-10 07:56:02 -0400

    fix: missing required primary type on block items (#81)

--- src/mdast2jcr/hb/partials/grid-table.js
+++ src/mdast2jcr/hb/partials/grid-table.js
@@ -378,7 +378,7 @@ function getBlockItems(mdast, modelHelper, definitions, allowedComponents) {
 
         extractProperties(row, fieldGroup.model, 'blockItem', component, fieldGroup.fields, properties);
 
-        items.push(`<item_${items.length} sling:resourceType="core/franklin/components/block/v1/block/item" name="${fieldGroup.model.id}" ${Object.entries(properties).map(([k, v]) => `${k}="${v}"`).join(' ')}></item_${items.length}>`);
+        items.push(`<item_${items.length} jcr:primaryType="nt:unstructured" sling:resourceType="core/franklin/components/block/v1/block/item" name="${fieldGroup.model.id}" ${Object.entries(properties).map(([k, v]) => `${k}="${v}"`).join(' ')}></item_${items.length}>`);
       }
     } else {
       // const msg = `The component '${componentId}' is not allowed in the block
